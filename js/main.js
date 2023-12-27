e0b6c444fbd764f865a0dbe9df8d6fee1427d9ce
Tahir Zaman <tahirunity786@gmail.com>
2023-12-27 22:52:22 +0500

    Fourth Commit

--- js/main.js
+++ js/main.js
@@ -40,22 +40,3 @@ $(window).on('load', function() {
 feather.replace()
 
 // Testimonial
-$('#testi-clients').owlCarousel({
-    loop: true,
-    center: true,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    margin: 20,
-    nav: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 2
-        },
-        1000: {
-            items: 3
-        }
-    }
-})
